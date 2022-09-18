d8963156411679cc7e5648d796898e3e1d150f89
unknown <you@example.com>
2022-09-18 14:35:14 +0100

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+
+function Home() {
+  return (
+    <>
+      <section className='heading'>
+        <h1>Welcome to house marketplace</h1>
+        <p>Hope you enjoy shopping with us</p>
+      </section>
+
+    
+    </>
+  )
+}
+
+export default Home
